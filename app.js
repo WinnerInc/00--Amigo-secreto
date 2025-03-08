afea7702c1fa930e0858b62e28b4fee069ad4397
WinnerInc <rretamalg@gmail.com>
2025-03-07 22:58:39 -0300

    cambio nombe funcion d eacuerdo a html

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ let Amigos_Ingresados = [];
 
 // cosas que se requieren hacer 
 // 1. se debe ingresar el nombre de los amigos secretos
-function Ingresar_Amigos_Secretos() {
+function agregarAmigo() {
     let NombreAmigo = document.getelementById("amigo");
     let amigo = NombreAmigo.value;
         
     if (amigo === "") {
-        alert("Debes ingresar un nombre"); // 2. se debe validar que el nombre no este vacio
+        alert("Debes ingresar un nombre");
+        return; // 2. se debe validar que el nombre no este vacio
         // 3. se debe validar que el nombre ingresado no exista en el arreglo
         // 4. se debe mostrar un mensaje de error si el nombre ya existe
     } else if (Amigos_Ingresados.includes(amigo)) {
         alert("El nombre ya existe");
+        return;
         // 5. se debe mostrar un mensaje de exito si el nombre se agrego correctamente
     } else {
         Amigos_Ingresados.push(amigo);
         alert("Amigo ingresado correctamente");
     }
-    console.log(Amigos_Ingresados); }
+    console.log(Amigos_Ingresados); 
+}
 
 
 
